1f9074273cb32eb7fc070bba7972c8215072c025
dexitional <ebenezerkb.ackah@gmail.com>
2025-03-20 10:26:16 +0000

    initial

--- components/FooterSideLeft.tsx
+++ components/FooterSideLeft.tsx
@@ -8,19 +8,22 @@ type Props = {}
 
 function FooterSideLeft({}: Props) {
   return (
-    <div className="pb-10 sm:pb-0 sm:pr-3 sm:min-h-48 sm:min-w-[23rem] flex flex-col space-y-6 sm:space-y-10 sm:border-r-4 border-b-4 sm:border-b-0 border-dashed border-white">
-        <h1 className="text-xl sm:text-[1.75rem] leading-5 sm:leading-8 font-black">Nursing and Midwifery Training College - Esiama.</h1>
+    <div className="pb-10 sm:pb-0 sm:pr-3 sm:min-h-48 sm:min-w-[23rem] flex flex-col space-y-6 sm:space-y-6 sm:border-r-4 border-b-4 sm:border-b-0 border-dashed border-white">
+        <div className="space-y-3">
+            <h1 className="text-xl sm:text-[1.7rem] leading-5 sm:leading-8 font-black text-justify">Nursing and Midwifery Training College - Esiama.</h1>
+            <p className="text-sm sm:text-lg leading-5 sm:leading-8 font-black">P.O.Box 7, Esiama W/R, Ghana, West Africa.     </p>
+        </div>
         <div className="flex items-center justify-start space-x-4">
-            <div className="p-3 h-fit w-fit bg-black rounded-full">
+            <div className="p-3 h-fit w-fit bg-primary rounded-full">
                 <FaInstagram className="h-4 w-4 sm:h-6 sm:w-6 text-white"/>
             </div>
-            <div className="p-3 h-fit w-fit bg-black rounded-full">
+            <div className="p-3 h-fit w-fit bg-primary rounded-full">
                 <FaFacebookF className="h-4 w-4 sm:h-6 sm:w-6 text-white"/>
             </div>
-            <div className="p-3 h-fit w-fit bg-black rounded-full">
+            <div className="p-3 h-fit w-fit bg-primary rounded-full">
                 <FaXTwitter className="h-4 w-4 sm:h-6 sm:w-6 text-white"/>
             </div>
-            <div className="p-3 h-fit w-fit bg-black rounded-full">
+            <div className="p-3 h-fit w-fit bg-primary rounded-full">
                 <FaYoutube className="h-4 w-4 sm:h-6 sm:w-6 text-white"/>
             </div>
         </div>
--- components/FooterSideMid.tsx
+++ components/FooterSideMid.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { MdEmail, MdPlayArrow } from 'react-icons/md'
 
@@ -8,25 +9,25 @@ function FooterSideMid({}: Props) {
   return (
     <div className="pb-10 sm:pb-0 sm:px-10 sm:min-h-48 sm:min-w-[27rem] flex flex-col justify-start space-y-6 border-b-4 sm:border-b-0 sm:border-r-4 border-dashed border-white">
         <div className="flex flex-col justify-start space-y-3 sm:space-y-6">
-            <div className="flex items-center space-x-1 ">
-                <span className="text-lg font-bold underline">Our Vision and Values</span>
+            <Link href="" className="flex items-center space-x-1 ">
+                <span className="text-lg font-bold underline">Our Mission, Vision and Values</span>
                 <MdPlayArrow className="h-6 w-6"/>
-            </div>
-            <div className="flex items-center space-x-1 ">
-                <span className="text-lg font-bold underline">Discover Our Category of Services</span>
+            </Link>
+            <Link href="" className="flex items-center space-x-1 ">
+                <span className="text-lg font-bold underline">Discover Our Programs of Study</span>
                 <MdPlayArrow className="h-6 w-6"/>
-            </div>
-            <div className="flex items-center space-x-1 ">
-                <span className="text-lg font-bold underline">Browse Through Our Products </span>
+            </Link>
+            <Link href="" className="flex items-center space-x-1 ">
+                <span className="text-lg font-bold underline">Acquaint with Our History </span>
                 <MdPlayArrow className="h-6 w-6"/>
-            </div>
+            </Link>
             <div className="flex items-center space-x-1 ">
                 <span className="text-lg font-bold underline">Contact Us</span>
                 <MdPlayArrow className="h-6 w-6"/>
             </div>
         </div>
-        <div className="px-6 py-3 w-56 bg-[#EE3E02] flex items-center justify-center space-x-2">
-            <span className="text-white text-lg font-bold tracking-wider">Service Request</span>
+        <div className="px-6 py-3 w-56 bg-[rgb(250,149,10)] flex items-center justify-center space-x-2">
+            <span className="text-white text-lg font-bold tracking-wider">MOH Portal</span>
             <MdPlayArrow className="h-5 w-5 text-white"/>
         </div>
     </div>
